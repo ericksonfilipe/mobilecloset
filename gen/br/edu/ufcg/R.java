b33895f27c6ad551eab0928e7c9810fb1262df8a
Janderson <janderson.jason@gmail.com>
2011-09-21 00:23:57 -0300

    efeito nas gavetas do menu principal

--- gen/br/edu/ufcg/R.java
+++ gen/br/edu/ufcg/R.java
@@ -12,9 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int cabide=0x7f020001;
-        public static final int gaveta=0x7f020002;
-        public static final int icon=0x7f020003;
+        public static final int buttonhighlight=0x7f020001;
+        public static final int cabide=0x7f020002;
+        public static final int gaveta=0x7f020003;
+        public static final int gaveta_aberta=0x7f020004;
+        public static final int icon=0x7f020005;
     }
     public static final class id {
         public static final int buttonEscolherRoupas=0x7f050007;
