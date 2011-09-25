1e6e865bcae51e9161dfdc121e45acd5ed520c1f
Janderson <janderson.jason@gmail.com>
2011-09-25 11:54:53 -0300

    ideia de menu interno

--- gen/br/edu/ufcg/R.java
+++ gen/br/edu/ufcg/R.java
@@ -18,36 +18,49 @@ public final class R {
         public static final int botao_creditos_aberto=0x7f020004;
         public static final int botao_instrucoes=0x7f020005;
         public static final int botao_instrucoes_aberto=0x7f020006;
-        public static final int botao_meuguardaroupa=0x7f020007;
-        public static final int botao_meuguardaroupa_aberto=0x7f020008;
-        public static final int botao_sair=0x7f020009;
-        public static final int botao_sair_aberto=0x7f02000a;
-        public static final int button_bluetooth=0x7f02000b;
-        public static final int button_creditos=0x7f02000c;
-        public static final int button_instrucoes=0x7f02000d;
-        public static final int button_meuguardaroupa=0x7f02000e;
-        public static final int button_sair=0x7f02000f;
-        public static final int cabide=0x7f020010;
-        public static final int creditos=0x7f020011;
-        public static final int icon=0x7f020012;
+        public static final int botao_manequim=0x7f020007;
+        public static final int botao_manequim_aberto=0x7f020008;
+        public static final int botao_meuguardaroupa=0x7f020009;
+        public static final int botao_meuguardaroupa_aberto=0x7f02000a;
+        public static final int botao_provar=0x7f02000b;
+        public static final int botao_provar_aberto=0x7f02000c;
+        public static final int botao_roupas=0x7f02000d;
+        public static final int botao_roupas_aberto=0x7f02000e;
+        public static final int botao_sair=0x7f02000f;
+        public static final int botao_sair_aberto=0x7f020010;
+        public static final int button_bluetooth=0x7f020011;
+        public static final int button_creditos=0x7f020012;
+        public static final int button_instrucoes=0x7f020013;
+        public static final int button_manequim=0x7f020014;
+        public static final int button_meuguardaroupa=0x7f020015;
+        public static final int button_provar=0x7f020016;
+        public static final int button_roupas=0x7f020017;
+        public static final int button_sair=0x7f020018;
+        public static final int cabide=0x7f020019;
+        public static final int creditos=0x7f02001a;
+        public static final int icon=0x7f02001b;
     }
     public static final class id {
-        public static final int buttonEscolherRoupas=0x7f050002;
-        public static final int button_creditos=0x7f050006;
-        public static final int button_sair=0x7f050007;
-        public static final int button_sobre=0x7f050004;
-        public static final int linearLayout1=0x7f050003;
-        public static final int linearLayout2=0x7f050005;
+        public static final int buttonEscolherRoupas=0x7f050005;
+        public static final int button_creditos=0x7f050009;
+        public static final int button_manequim=0x7f050001;
+        public static final int button_provar=0x7f050003;
+        public static final int button_roupas=0x7f050002;
+        public static final int button_sair=0x7f05000a;
+        public static final int button_sobre=0x7f050007;
+        public static final int linearLayout1=0x7f050006;
+        public static final int linearLayout2=0x7f050008;
         public static final int surface_camera=0x7f050000;
-        public static final int textView1=0x7f050001;
-        public static final int textView2=0x7f050008;
+        public static final int textView1=0x7f050004;
+        public static final int textView2=0x7f05000b;
     }
     public static final class layout {
         public static final int captura_foto=0x7f030000;
-        public static final int creditos=0x7f030001;
-        public static final int escolherroupas=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int sobre=0x7f030004;
+        public static final int closet=0x7f030001;
+        public static final int creditos=0x7f030002;
+        public static final int escolherroupas=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int sobre=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
