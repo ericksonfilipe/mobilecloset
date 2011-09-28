4da7199e2c02d5a83dadc6ae76cde57243770790
Izabela <izabela.vmelo@gmail.com>
2011-09-28 02:00:53 -0300

    camera tirando foto... mas nao ta salvando ainda

---
+++ src/br/edu/ufcg/TirarFotoActivity.java
@@ -0,0 +1,54 @@
+package br.edu.ufcg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class TirarFotoActivity extends Activity implements ImageListener, OnClickListener {
+
+	private CameraView mPreview;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_preview);
+		mPreview = (CameraView) findViewById(R.id.imagem_camera);
+		mPreview.addImageListener(this);
+		
+		Button tiraFoto = (Button) findViewById(R.id.button_camera);
+		tiraFoto.setOnClickListener(this);
+	}
+
+	public void takeImage(byte[] image) { 
+		 // Cleaning memory 
+		 System.gc(); 
+		 mPreview.setVisibility(View.INVISIBLE);
+		 mPreview.freezeCamera();
+
+		 //try {
+		 //	OutputStream fos = openFileOutput(File.separator + "var" + File.separator + "mobileCloset" + File.separator + "imagem.jpg", Context.MODE_WORLD_READABLE);
+		 //Log.e("pffffffff", Environment.getExternalStorageDirectory()+"");
+		 //fos.write(image);
+		 //fos.flush();
+		 //fos.close();
+			
+		 //} catch (FileNotFoundException e) {
+		 //e.printStackTrace();
+		 //} catch (IOException e) {
+		 //	e.printStackTrace();
+		 //}
+		 
+		 this.finish();
+	}
+
+	public void onClick(View v) {
+			mPreview.takePicture();
+		
+	}
+	
+	
+
+}
