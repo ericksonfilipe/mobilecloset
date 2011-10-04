3cf90bd8a4bdf548f89ddb3c33da0c2f99080190
Erickson <ericksonfilipe@gmail.com>
2011-10-04 19:01:44 -0300

    adicionando o import =P

--- src/br/edu/ufcg/TirarFotoActivity.java
+++ src/br/edu/ufcg/TirarFotoActivity.java
@@ -1,6 +1,7 @@
 package br.edu.ufcg;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,46 +19,46 @@ public class TirarFotoActivity extends Activity implements ImageListener, OnClic
 		setContentView(R.layout.camera_preview);
 		mPreview = (CameraView) findViewById(R.id.imagem_camera);
 		mPreview.addImageListener(this);
-		
+
 		Button tiraFoto = (Button) findViewById(R.id.button_camera);
 		tiraFoto.setOnClickListener(this);
-      
-      this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		
+
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
 		this.dh = new DataHelper(this);
-		
+
 	}
 
 	public void takeImage(byte[] image) { 
-		 // Cleaning memory 
-		 System.gc(); 
-		 mPreview.setVisibility(View.INVISIBLE);
-		 mPreview.freezeCamera();
-		 
-		 this.dh.salvarImagemMemoriaExterna(image);
-		 System.out.println("xxxxxxxxxxxxxxxxx FOI");
+		// Cleaning memory 
+		System.gc(); 
+		mPreview.setVisibility(View.INVISIBLE);
+		mPreview.freezeCamera();
 
-		 //try {
-		 //	OutputStream fos = openFileOutput(File.separator + "var" + File.separator + "mobileCloset" + File.separator + "imagem.jpg", Context.MODE_WORLD_READABLE);
-		 //Log.e("pffffffff", Environment.getExternalStorageDirectory()+"");
-		 //fos.write(image);
-		 //fos.flush();
-		 //fos.close();
-			
-		 //} catch (FileNotFoundException e) {
-		 //e.printStackTrace();
-		 //} catch (IOException e) {
-		 //	e.printStackTrace();
-		 //}
-		 
-		 this.finish();
+		this.dh.salvarImagemMemoriaExterna(image);
+		System.out.println("xxxxxxxxxxxxxxxxx FOI");
+
+		//try {
+		//	OutputStream fos = openFileOutput(File.separator + "var" + File.separator + "mobileCloset" + File.separator + "imagem.jpg", Context.MODE_WORLD_READABLE);
+		//Log.e("pffffffff", Environment.getExternalStorageDirectory()+"");
+		//fos.write(image);
+		//fos.flush();
+		//fos.close();
+
+		//} catch (FileNotFoundException e) {
+		//e.printStackTrace();
+		//} catch (IOException e) {
+		//	e.printStackTrace();
+		//}
+
+		this.finish();
 	}
 
 	public void onClick(View v) {
-			mPreview.takePicture();
-		
+		mPreview.takePicture();
+
 	}
-	
-	
+
+
 
 }
