3e8a17359c476d8399b02022045ecbcd12a53141
Janderson <janderson.jason@gmail.com>
2011-10-05 23:44:44 -0300

    mostrando no manequim a ultima foto tirada

--- src/br/edu/ufcg/Provar.java
+++ src/br/edu/ufcg/Provar.java
@@ -5,7 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Provar extends Activity {
-	 /** Called when the activity is first created. */
+         /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
