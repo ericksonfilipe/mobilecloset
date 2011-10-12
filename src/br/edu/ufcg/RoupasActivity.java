faafa8cc35c96d299744f743cd1859317b978eaa
Izabela <izabela.vmelo@gmail.com>
2011-10-12 15:34:11 -0300

    criando as activity para o menu de salvar roupas

--- src/br/edu/ufcg/RoupasActivity.java
+++ src/br/edu/ufcg/RoupasActivity.java
@@ -8,22 +8,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Closet extends Activity implements OnClickListener {
+public class RoupasActivity extends Activity implements OnClickListener {
+	
 	 /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.closet);
+        setContentView(R.layout.roupas);
         
-		Button bManequim = (Button) findViewById(R.id.button_manequim);
+		Button bManequim = (Button) findViewById(R.id.button_cadastrar_roupa);
 		bManequim.setOnClickListener(this);
 
-		Button bRoupas = (Button) findViewById(R.id.button_roupas);
+		Button bRoupas = (Button) findViewById(R.id.button_ver_roupas);
 		bRoupas.setOnClickListener(this);
-		
-		Button bProvar = (Button) findViewById(R.id.button_provar);
-		bProvar.setOnClickListener(this);
-		
+
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         
     }
@@ -31,17 +29,13 @@ public class Closet extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		Intent i;
 		switch (v.getId()) {
-		case R.id.button_manequim:
-			i = new Intent(v.getContext(), ManequimActivity.class);
+		case R.id.button_cadastrar_roupa:
+			i = new Intent(v.getContext(), TirarFotoRoupaActivity.class);
 			startActivity(i);
 			break;
-		case R.id.button_roupas:
-			i = new Intent(v.getContext(), RoupasActivity.class);
-			startActivity(i);
-			break;
-		case R.id.button_provar:
-			i = new Intent(v.getContext(), Provar.class);
-			startActivity(i);
+		case R.id.button_ver_roupas:
+			//i = new Intent(v.getContext(), CapturaFoto.class);
+			//startActivity(i);
 			break;
 		}
 		
---
+++ src/br/edu/ufcg/TirarFotoRoupaActivity.java
@@ -0,0 +1,69 @@
+package br.edu.ufcg;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import br.edu.ufcg.BD.BDAdapter;
+
+public class TirarFotoRoupaActivity extends Activity implements ImageListener, OnClickListener {
+
+	private CameraView mPreview;
+	private BDAdapter dh;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_roupas);
+		mPreview = (CameraView) findViewById(R.id.imagem_camera_roupas);
+		mPreview.addImageListener(this);
+		
+		Button tiraFoto = new Button(this);
+		tiraFoto.setBackgroundColor(Color.TRANSPARENT);
+		tiraFoto.setOnClickListener(this);
+		addContentView(tiraFoto, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		this.dh = new BDAdapter(this);
+
+	}
+
+	public void takeImage(byte[] image) { 
+		// Cleaning memory 
+		System.gc(); 
+		mPreview.setVisibility(View.INVISIBLE);
+		mPreview.freezeCamera();
+
+		this.dh.salvarImagemMemoriaExterna(image);
+		System.out.println("xxxxxxxxxxxxxxxxx FOI");
+
+		//try {
+		//	OutputStream fos = openFileOutput(File.separator + "var" + File.separator + "mobileCloset" + File.separator + "imagem.jpg", Context.MODE_WORLD_READABLE);
+		//Log.e("pffffffff", Environment.getExternalStorageDirectory()+"");
+		//fos.write(image);
+		//fos.flush();
+		//fos.close();
+
+		//} catch (FileNotFoundException e) {
+		//e.printStackTrace();
+		//} catch (IOException e) {
+		//	e.printStackTrace();
+		//}
+
+		this.finish();
+	}
+
+	public void onClick(View v) {
+		mPreview.takePicture();
+
+	}
+
+
+}
