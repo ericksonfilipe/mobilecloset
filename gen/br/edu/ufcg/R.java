eb78c15fa1259a02d1141f14839cf0e52052ebce
Erickson <ericksonfilipe@gmail.com>
2011-10-12 12:54:01 -0300

    tentativa de visualizar varias fotos

--- gen/br/edu/ufcg/R.java
+++ gen/br/edu/ufcg/R.java
@@ -49,23 +49,22 @@ public final class R {
         public static final int tracejado2=0x7f020023;
     }
     public static final class id {
-        public static final int ImageManequim=0x7f050006;
-        public static final int LinearLayoutImg=0x7f050005;
-        public static final int buttonEscolherRoupas=0x7f050007;
-        public static final int button_creditos=0x7f05000c;
-        public static final int button_escolher_manequim=0x7f05000f;
+        public static final int buttonEscolherRoupas=0x7f050006;
+        public static final int button_creditos=0x7f05000b;
+        public static final int button_escolher_manequim=0x7f05000e;
         public static final int button_manequim=0x7f050002;
         public static final int button_provar=0x7f050004;
         public static final int button_roupas=0x7f050003;
-        public static final int button_sair=0x7f05000d;
-        public static final int button_sobre=0x7f05000a;
-        public static final int button_tirar_foto=0x7f05000e;
-        public static final int dropaBD=0x7f050009;
+        public static final int button_sair=0x7f05000c;
+        public static final int button_sobre=0x7f050009;
+        public static final int button_tirar_foto=0x7f05000d;
+        public static final int dropaBD=0x7f050008;
+        public static final int gallery=0x7f050005;
         public static final int imageView1=0x7f050000;
         public static final int imagem_camera=0x7f050001;
-        public static final int linearLayout1=0x7f050008;
-        public static final int linearLayout2=0x7f05000b;
-        public static final int textView2=0x7f050010;
+        public static final int linearLayout1=0x7f050007;
+        public static final int linearLayout2=0x7f05000a;
+        public static final int textView2=0x7f05000f;
     }
     public static final class layout {
         public static final int camera_preview=0x7f030000;
@@ -81,4 +80,25 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello=0x7f040001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a HelloGallery.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #HelloGallery_android_galleryItemBackground br.edu.ufcg:android_galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #HelloGallery_android_galleryItemBackground
+         */
+        public static final int[] HelloGallery = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link br.edu.ufcg.R.attr#android_galleryItemBackground}
+          attribute's value can be found in the {@link #HelloGallery} array.
+          @attr name android:android_galleryItemBackground
+        */
+        public static final int HelloGallery_android_galleryItemBackground = 0;
+    };
 }
--- src/br/edu/ufcg/EscolherManequimActivity.java
+++ src/br/edu/ufcg/EscolherManequimActivity.java
@@ -4,16 +4,26 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-
-import br.edu.ufcg.BD.BDAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
+import br.edu.ufcg.BD.BDAdapter;
+import br.edu.ufcg.model.Manequim;
 
 public class EscolherManequimActivity extends Activity {
        
@@ -24,22 +34,85 @@ public class EscolherManequimActivity extends Activity {
         
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         
-        ImageView imgView =(ImageView)findViewById(R.id.ImageManequim);
-        BDAdapter bd = new BDAdapter(this);
         
-        //por enquanto, pegando ultimo manequim 
-        String caminhoManequim = bd.getAllManequins().get(bd.getAllManequins().size()-1).getPath();
         
-        Drawable drawable = LoadImage(Environment.getExternalStoragePublicDirectory(
-        		Environment.DIRECTORY_PICTURES) + File.separator + caminhoManequim);
-        
-        Log.e("drawable", "drawable null? " + drawable); //TIRAR
-        
-        imgView.setImageDrawable(drawable);
+        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+	    gallery.setAdapter(new ImageAdapter(this));
+
+	    gallery.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView parent, View v, int position, long id) {
+	            Toast.makeText(EscolherManequimActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    
+//        
+//        ImageView imgView =(ImageView)findViewById(R.id.ImageManequim);
+//        BDAdapter bd = new BDAdapter(this);
+//        
+//        //por enquanto, pegando ultimo manequim 
+//        String caminhoManequim = bd.getAllManequins().get(bd.getAllManequins().size()-1).getPath();
+//        
+//        Drawable drawable = LoadImage(Environment.getExternalStoragePublicDirectory(
+//        		Environment.DIRECTORY_PICTURES) + File.separator + caminhoManequim);
+//        
+//        Log.e("drawable", "drawable null? " + drawable); //TIRAR
+//        
+//        imgView.setImageDrawable(drawable);
         
     }
+	
+	public class ImageAdapter extends BaseAdapter {
+	    int mGalleryItemBackground;
+	    private Context mContext;
+
+	    private String[] imagens;
+
+	    public ImageAdapter(Context c) {
+	        mContext = c;
+	        TypedArray attr = mContext.obtainStyledAttributes(R.styleable.HelloGallery);
+	        mGalleryItemBackground = attr.getResourceId(
+	                R.styleable.HelloGallery_android_galleryItemBackground, 0);
+	        attr.recycle();
+	        
+	        carregaArrayImagens();
+	    }
+
+	    public int getCount() {
+	        return imagens.length;
+	    }
+
+	    public Object getItem(int position) {
+	        return position;
+	    }
+
+	    public long getItemId(int position) {
+	        return position;
+	    }
+
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        ImageView imageView = new ImageView(mContext);
+
+	        Drawable drawable = carregarImagem(Environment.getExternalStoragePublicDirectory(
+	        		Environment.DIRECTORY_PICTURES) + File.separator + imagens[position]);
+	        imageView.setImageDrawable(drawable);
+	        imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
+	        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+	        imageView.setBackgroundResource(mGalleryItemBackground);
+
+	        return imageView;
+	    }
+	    
+	    private void carregaArrayImagens() {
+			BDAdapter bd = new BDAdapter(EscolherManequimActivity.this);
+			List<Manequim> manequins = bd.getAllManequins();
+			imagens = new String[manequins.size()];
+			for (int i = 0; i < manequins.size(); i++) {
+				imagens[i] = manequins.get(i).getPath();
+			}
+		}
+	}
     
-	private Drawable LoadImage(String src) {
+	private Drawable carregarImagem(String src) {
 	      try {
 	          InputStream is = new BufferedInputStream(new FileInputStream(src));
 	          Drawable d = Drawable.createFromStream(is, "src name");
