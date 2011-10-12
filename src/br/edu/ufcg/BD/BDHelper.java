2286b8c279c53105364c9519ee0508f566076970
Erickson <ericksonfilipe@gmail.com>
2011-10-12 18:51:31 -0300

    modificando o banco de dados para ter manequim padr?o

--- src/br/edu/ufcg/BD/BDHelper.java
+++ src/br/edu/ufcg/BD/BDHelper.java
@@ -9,10 +9,11 @@ public class BDHelper extends SQLiteOpenHelper{
 
 	private static final String NOME_DB = "mobileCloset";
 
-	private static final int VERSAO_DB = 2;
+	private static final int VERSAO_DB = 3;
 
-	private static final String SQL_CRIA_MANEQUIM = "CREATE TABLE manequim (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+	private final String SQL_CRIA_MANEQUIM = "CREATE TABLE manequim (id INTEGER PRIMARY KEY AUTOINCREMENT, "
 		+ "path TEXT NOT NULL);";
+	private final String SQL_CRIA_MANEQUIM_PADRAO = "CREATE TABLE manequim_padrao (caminho_manequim TEXT PRIMARY KEY);";
 
 
 	public BDHelper(Context context) {
@@ -22,12 +23,13 @@ public class BDHelper extends SQLiteOpenHelper{
 	@Override
 	public void onCreate(SQLiteDatabase bd) {
 		bd.execSQL(SQL_CRIA_MANEQUIM);
+		bd.execSQL(SQL_CRIA_MANEQUIM_PADRAO);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase bd, int oldVersion, int newVersion) {
+		bd.execSQL("DROP TABLE IF EXISTS manequim_padrao;");
 		bd.execSQL("DROP TABLE IF EXISTS manequim;");
 		this.onCreate(bd);
 	}
-
 }
