e7dc926efb2e0e088fdf2c8814c35c80e616657a
Erickson <ericksonfilipe@gmail.com>
2011-10-23 19:16:44 -0300

    modificando os dados de calibragem. J? est? salvando no banco de dados, ao apertar o bot?o central (continua bugando a parte dos bot?es)

--- src/br/edu/ufcg/model/Calibragem.java
+++ src/br/edu/ufcg/model/Calibragem.java
@@ -3,20 +3,20 @@ package br.edu.ufcg.model;
 public class Calibragem {
 
 	private Categoria categoria;
-	private Double posicaoX;
-	private Double posicaoY;
-	private Double altura;
-	private Double largura;
+	public Integer left;
+	public Integer top;
+	public Integer right;
+	public Integer bottom;
 
 	public Calibragem() {}
 
-	public Calibragem(Categoria categoria, Double posicaoX, Double posicaoY,
-			Double altura, Double largura) {
+	public Calibragem(Categoria categoria, Integer left, Integer top,
+			Integer right, Integer bottom) {
 		this.categoria = categoria;
-		this.posicaoX = posicaoX;
-		this.posicaoY = posicaoY;
-		this.altura = altura;
-		this.largura = largura;
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
 	}
 
 	public Categoria getCategoria() {
@@ -27,40 +27,8 @@ public class Calibragem {
 		this.categoria = categoria;
 	}
 
-	public Double getPosicaoX() {
-		return posicaoX;
-	}
-
-	public void setPosicaoX(Double posicaoX) {
-		this.posicaoX = posicaoX;
-	}
-
-	public Double getPosicaoY() {
-		return posicaoY;
-	}
-
-	public void setPosicaoY(Double posicaoY) {
-		this.posicaoY = posicaoY;
-	}
-
-	public Double getAltura() {
-		return altura;
-	}
-
-	public void setAltura(Double altura) {
-		this.altura = altura;
-	}
-
-	public Double getLargura() {
-		return largura;
-	}
-
-	public void setLargura(Double largura) {
-		this.largura = largura;
-	}
-
 	@Override
 	public String toString() {
-		return "Categoria = " + categoria + ", posx = " + posicaoX + ", posy = " + posicaoY;
+		return String.format("%s - %d %d %d %d", categoria, left, top, right, bottom);
 	}
 }
