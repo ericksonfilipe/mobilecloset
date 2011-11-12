25957af4e6d96842ab1c966f4ed1b27f8275b674
Erickson <ericksonfilipe@gmail.com>
2011-11-12 18:41:45 -0300

    botoes na tela de provar

--- gen/br/edu/ufcg/R.java
+++ gen/br/edu/ufcg/R.java
@@ -91,27 +91,27 @@ public final class R {
         public static final int zoom_out=0x7f02004d;
     }
     public static final class id {
-        public static final int alterarDimensoes=0x7f06001c;
+        public static final int alterarDimensoes=0x7f060022;
         public static final int buttonEscolherRoupas=0x7f060007;
-        public static final int button_bermudas=0x7f060017;
-        public static final int button_cadastrar_roupa=0x7f060010;
-        public static final int button_calcas=0x7f060018;
-        public static final int button_camisas=0x7f06001a;
-        public static final int button_camisas_manga_comprida=0x7f060013;
-        public static final int button_camisas_manga_curta=0x7f060014;
-        public static final int button_camisas_sem_manga=0x7f060015;
+        public static final int button_bermudas=0x7f06001d;
+        public static final int button_cadastrar_roupa=0x7f060016;
+        public static final int button_calcas=0x7f06001e;
+        public static final int button_camisas=0x7f060020;
+        public static final int button_camisas_manga_comprida=0x7f060019;
+        public static final int button_camisas_manga_curta=0x7f06001a;
+        public static final int button_camisas_sem_manga=0x7f06001b;
         public static final int button_creditos=0x7f06000c;
         public static final int button_escolher_manequim=0x7f06000f;
         public static final int button_manequim=0x7f060003;
         public static final int button_provar=0x7f060005;
         public static final int button_roupas=0x7f060004;
-        public static final int button_saias=0x7f060019;
+        public static final int button_saias=0x7f06001f;
         public static final int button_sair=0x7f06000d;
-        public static final int button_shorts=0x7f060016;
+        public static final int button_shorts=0x7f06001c;
         public static final int button_sobre=0x7f06000a;
         public static final int button_tirar_foto=0x7f06000e;
-        public static final int button_ver_roupas=0x7f060011;
-        public static final int button_vestidos=0x7f06001b;
+        public static final int button_ver_roupas=0x7f060017;
+        public static final int button_vestidos=0x7f060021;
         public static final int dropaBD=0x7f060009;
         public static final int gallery=0x7f060006;
         public static final int imageView1=0x7f060000;
@@ -119,8 +119,14 @@ public final class R {
         public static final int imagem_camera_roupas=0x7f060002;
         public static final int linearLayout1=0x7f060008;
         public static final int linearLayout2=0x7f06000b;
-        public static final int salvarCalibragem=0x7f06001d;
-        public static final int textView2=0x7f060012;
+        public static final int passar_mais=0x7f060014;
+        public static final int passar_maiss=0x7f060015;
+        public static final int passar_menos=0x7f060011;
+        public static final int passar_menoss=0x7f060012;
+        public static final int pbl=0x7f060010;
+        public static final int salvarCalibragem=0x7f060023;
+        public static final int textView2=0x7f060018;
+        public static final int xpto=0x7f060013;
     }
     public static final class layout {
         public static final int camera_preview=0x7f030000;
@@ -130,12 +136,13 @@ public final class R {
         public static final int escolher_manequim=0x7f030004;
         public static final int main=0x7f030005;
         public static final int manequim=0x7f030006;
-        public static final int provar=0x7f030007;
-        public static final int roupas=0x7f030008;
-        public static final int sobre=0x7f030009;
-        public static final int ver_camisas=0x7f03000a;
-        public static final int ver_roupas=0x7f03000b;
-        public static final int visualizacao_roupas=0x7f03000c;
+        public static final int provador_botoes=0x7f030007;
+        public static final int provar=0x7f030008;
+        public static final int roupas=0x7f030009;
+        public static final int sobre=0x7f03000a;
+        public static final int ver_camisas=0x7f03000b;
+        public static final int ver_roupas=0x7f03000c;
+        public static final int visualizacao_roupas=0x7f03000d;
     }
     public static final class menu {
         public static final int calibragem_menu=0x7f050000;
--- src/br/edu/ufcg/Creditos.java
+++ src/br/edu/ufcg/Creditos.java
@@ -1,30 +1,18 @@
 package br.edu.ufcg;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import br.edu.ufcg.BD.BDAdapter;
 
 public class Creditos extends Activity {
-	 /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.creditos);
-        
-//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        
-        BDAdapter dao = new BDAdapter(this);
-        byte[] pleas = dao.getPlecas();
-        Log.e("iai????????", String.valueOf(pleas));
-        Bitmap plecas = BitmapFactory.decodeByteArray(pleas, 0, pleas.length);
-        ImageView i = new ImageView(this);
-        i.setImageBitmap(plecas);
-        setContentView(i);
-        
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.creditos);
+
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+	}
 
 }
