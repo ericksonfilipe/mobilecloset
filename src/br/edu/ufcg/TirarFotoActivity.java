6939028e2b4a35c8e5ee04a399fe3ef8a7a4399d
Erickson <ericksonfilipe@gmail.com>
2011-11-14 13:09:57 -0300

    removendo imports n?o usados

--- src/br/edu/ufcg/TirarFotoActivity.java
+++ src/br/edu/ufcg/TirarFotoActivity.java
@@ -7,7 +7,6 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import br.edu.ufcg.BD.BDAdapter;
 
