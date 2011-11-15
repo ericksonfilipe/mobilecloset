801a960d83e7905baf1c691bb69f5ffe31b02d71
Izabela Melo <izabela.vmelo@gmail.com>
2011-11-15 15:42:05 -0300

    mudando visualizar roupas para n?o utilizar gallery

--- src/br/edu/ufcg/VisualizacaoDeRoupasActivity.java
+++ src/br/edu/ufcg/VisualizacaoDeRoupasActivity.java
@@ -5,42 +5,81 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import br.edu.ufcg.BD.BDAdapter;
 import br.edu.ufcg.model.Roupa;
 
 public class VisualizacaoDeRoupasActivity extends Activity {
 
-	Gallery gallery;
-	private ImageAdapter imageAdapter;
 	private BDAdapter dao;
+	private VisualizadorRoupa visualizadorRoupa;
+	public ImageButton voltaSuperiorButton;
+	public ImageButton proximaSuperiorButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.visualizacao_roupas);
 		
 		dao = new BDAdapter(this);
+		visualizadorRoupa = new VisualizadorRoupa(this, dao.getRoupas());
+		setContentView(visualizadorRoupa);
+		
+        proximaSuperiorButton = new ImageButton(this);
+        proximaSuperiorButton.setImageResource(R.drawable.next);
+		proximaSuperiorButton.setBackgroundColor(Color.TRANSPARENT);
+		proximaSuperiorButton.setOnClickListener(new ProximoListener());
 
-		gallery = (Gallery) findViewById(R.id.gallery);
-		imageAdapter = new ImageAdapter(this);
-		gallery.setAdapter(imageAdapter);
+		voltaSuperiorButton = new ImageButton(this);
+		voltaSuperiorButton.setImageResource(R.drawable.previous_cinza);
+		voltaSuperiorButton.setBackgroundColor(Color.TRANSPARENT);
+		voltaSuperiorButton.setOnClickListener(new VoltaListener());
+
+		RelativeLayout layout = new RelativeLayout(this);
+		LinearLayout linear = new LinearLayout(this);
+		linear.addView(voltaSuperiorButton);
+		linear.addView(proximaSuperiorButton);
+		linear.setGravity(Gravity.RIGHT);
+		linear.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		layout.addView(linear);
+		layout.setGravity(Gravity.BOTTOM);
+
+		addContentView(layout, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}
+
+	private class VoltaListener implements OnClickListener {
+
+		public void onClick(View arg0) {
+			visualizadorRoupa.voltaRoupaSuperior();
+		}
+		
+	}
+
+	private class ProximoListener implements OnClickListener {
+
+		public void onClick(View arg0) {
+			visualizadorRoupa.proximaRoupaSuperior();
+		}
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,70 +90,95 @@ public class VisualizacaoDeRoupasActivity extends Activity {
 
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		imageAdapter.removeImagem();
+		visualizadorRoupa.removeImagem();
 		return true;
 	}
 
-	public class ImageAdapter extends BaseAdapter {
-		int mGalleryItemBackground;
-		private Context mContext;
-		private Roupa[] imagens;
-		private int posicaoAtual;
+	public class VisualizadorRoupa extends View {
 
-		public ImageAdapter(Context c) {
-			mContext = c;
-			TypedArray attr = mContext.obtainStyledAttributes(R.styleable.HelloGallery);
-			mGalleryItemBackground = attr.getResourceId(
-					R.styleable.HelloGallery_android_galleryItemBackground, 0);
-			attr.recycle();
+		private List<Roupa> roupas;
 
-			carregaArrayImagens();
+		private int posicaoRoupa;
+
+		private Drawable roupaAtual;
+
+		public VisualizadorRoupa(Context context, List<Roupa> roupas) {
+			super(context);
+			this.roupas = roupas;
+			if (!roupas.isEmpty()) {
+				roupaAtual = carregaDrawable(roupas.get(posicaoRoupa).getImagem());
+				roupaAtual.setBounds(0, 0, roupaAtual.getIntrinsicWidth(), roupaAtual.getIntrinsicHeight());
+			}
+
+			setFocusable(true);
+			this.requestFocus();
 		}
 
-		public int getCount() {
-			return imagens.length;
-		}
-
-		public Roupa getItem(int position) {
-			List<Roupa> roupas = dao.getRoupas();
-			return roupas.get(position);
-		}
-
-		public long getItemId(int position) {
-			return position;
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			posicaoAtual = position;
-			ImageView imageView = new ImageView(mContext);
-
-			byte[] imagem = imagens[position].getImagem();
-			Bitmap d = BitmapFactory.decodeByteArray(imagem, 0, imagem.length);
-
-			Matrix matrix = new Matrix();
-			matrix.setRotate(90);
-			Bitmap girado = Bitmap.createBitmap(d, 0, 0, d.getWidth(), d.getHeight(), matrix, true);
-
-			imageView.setImageBitmap(girado);
-			imageView.setLayoutParams(new Gallery.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-			imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-			imageView.setBackgroundResource(mGalleryItemBackground);
-
-			return imageView;
-		}
-	
 		public void removeImagem() {
-			dao.removeRoupa(imagens[posicaoAtual]);
-			carregaArrayImagens();
-			notifyDataSetChanged();
+			dao.removeRoupa(roupas.get(posicaoRoupa));
+			roupas.remove(posicaoRoupa);
+			if (roupas.isEmpty()) {
+				finish();
+				return;
+			} else if (posicaoRoupa == roupas.size()) {
+				posicaoRoupa--;
+			}
+			Roupa roupa = roupas.get(posicaoRoupa);
+			roupaAtual = carregaDrawable(roupa.getImagem());
+			roupaAtual.setBounds(0, 0, getWidth(), getHeight());
+			invalidate();
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+			if (roupaAtual != null) {
+				roupaAtual.draw(canvas);
+			}
+		}
+
+		public void proximaRoupaSuperior() {
+			if (posicaoRoupa < roupas.size()-1) {
+				posicaoRoupa++;
+				Roupa roupa = roupas.get(posicaoRoupa);
+				roupaAtual = carregaDrawable(roupa.getImagem());
+				roupaAtual.setBounds(0, 0, getWidth(), getHeight());
+				invalidate();
+			}
+			atualizaImagensBotoes();
+		}
+
+		public void voltaRoupaSuperior() {
+			if (posicaoRoupa > 0) {
+				posicaoRoupa--;
+				Roupa roupa = roupas.get(posicaoRoupa);
+				roupaAtual = carregaDrawable(roupa.getImagem());
+				roupaAtual.setBounds(0, 0, getWidth(), getHeight());
+				invalidate();
+			}
+			atualizaImagensBotoes();
+		}
+
+		private void atualizaImagensBotoes() {
+			if (posicaoRoupa > 0) {
+				voltaSuperiorButton.setImageResource(R.drawable.previous);
+			} else {
+				voltaSuperiorButton.setImageResource(R.drawable.previous_cinza);
+			}
+
+			if (posicaoRoupa < roupas.size() - 1) {
+				proximaSuperiorButton.setImageResource(R.drawable.next);
+			} else {
+				proximaSuperiorButton.setImageResource(R.drawable.next_cinza);
+			}
 		}
 		
-		private void carregaArrayImagens() {
-			List<Roupa> roupas = dao.getRoupas();
-			imagens = new Roupa[roupas.size()];
-			for (int i = 0; i < roupas.size(); i++) {
-				imagens[i] = roupas.get(i);
-			}
+		private Drawable carregaDrawable(byte[] imagem) {
+			Bitmap b = BitmapFactory.decodeByteArray(imagem, 0, imagem.length);
+			Matrix matrix = new Matrix();
+			matrix.setRotate(90);
+			Bitmap girado = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), matrix, true);
+			return new BitmapDrawable(girado);
 		}
 	}
 }
