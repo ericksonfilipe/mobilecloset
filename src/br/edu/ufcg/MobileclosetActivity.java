b48b994d03db92bb8310beae508107e9d8c912a3
Izabela Melo <izabela.vmelo@gmail.com>
2011-11-15 16:29:27 -0300

    Mudando a pagina principal. Falta mudar as imagens.

--- src/br/edu/ufcg/MobileclosetActivity.java
+++ src/br/edu/ufcg/MobileclosetActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ufcg;
 
+import br.edu.ufcg.BD.BDAdapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MobileclosetActivity extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
@@ -16,14 +18,17 @@ public class MobileclosetActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button bEscolher = (Button) findViewById(R.id.buttonEscolherRoupas);
-		bEscolher.setOnClickListener(this);
+		Button bManequim = (Button) findViewById(R.id.button_manequim);
+		bManequim.setOnClickListener(this);
+
+		Button bRoupas = (Button) findViewById(R.id.button_roupas);
+		bRoupas.setOnClickListener(this);
+		
+		Button bProvar = (Button) findViewById(R.id.button_provar);
+		bProvar.setOnClickListener(this);
 
 		Button bSair = (Button) findViewById(R.id.button_sair);
 		bSair.setOnClickListener(this);
-
-		Button bSobre = (Button) findViewById(R.id.button_sobre);
-		bSobre.setOnClickListener(this);
 
 		Button bCreditos = (Button) findViewById(R.id.button_creditos);
 		bCreditos.setOnClickListener(this);
@@ -35,16 +40,28 @@ public class MobileclosetActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		Intent i;
 		switch (v.getId()) {
-		case R.id.buttonEscolherRoupas:
-			i = new Intent(v.getContext(), Closet.class);
+		case R.id.button_manequim:
+			i = new Intent(v.getContext(), ManequimActivity.class);
 			startActivity(i);
+			break;
+		case R.id.button_roupas:
+			i = new Intent(v.getContext(), RoupasActivity.class);
+			startActivity(i);
+			break;
+		case R.id.button_provar:
+			i = new Intent(v.getContext(), ProvadorActivity.class);
+			BDAdapter dao = new BDAdapter(this);
+			if (dao.getManequimPadrao() == null) {
+//				i.putExtra("manequimFaltando", true);
+//				startActivity(i);
+				Toast.makeText(this, "Não há manequim escolhido!", Toast.LENGTH_LONG).show();
+			} else {
+				i.putExtra("background", dao.getManequimPadrao());
+				startActivity(i);
+			}
 			break;
 		case R.id.button_creditos:
 			i = new Intent(v.getContext(), Creditos.class);
-			startActivity(i);
-			break;
-		case R.id.button_sobre:
-			i = new Intent(v.getContext(), SobreActivity.class);
 			startActivity(i);
 			break;
 		case R.id.button_sair:
