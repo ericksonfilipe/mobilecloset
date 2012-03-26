4ee101419bb5c41c81b5c8be7c5f76f3cc86588d
Izabela <izabela.vmelo@gmail.com>
2012-03-26 14:27:08 -0300

    dando push pra Erickson tentar fazer... =/

--- src/br/edu/ufcg/TesteDeteccaoFace.java
+++ src/br/edu/ufcg/TesteDeteccaoFace.java
@@ -15,10 +15,13 @@ import android.media.FaceDetector.Face;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 
 public class TesteDeteccaoFace extends Activity {
 	private br.edu.ufcg.TesteDeteccaoFace.MyImageView myImageView;
 	private Face[] arrayFaces;
+	private LinearLayout layout;
 
 
 	/** Called when the activity is first created. */
@@ -26,17 +29,8 @@ public class TesteDeteccaoFace extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		BitmapDrawable bd = carregaDrawable();
-		myImageView = new MyImageView(this);
-		myImageView.setBackgroundDrawable(bd);
-		setContentView(myImageView);
-
-		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-	}
-	
-	private BitmapDrawable carregaDrawable() {
-		 Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.pessoa2);
+		
+		Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.pessoa2);
 		 
 		 FaceDetector f = new FaceDetector(b.getWidth(), b.getHeight(), 1);
 		 arrayFaces = new Face[1];
@@ -49,8 +43,53 @@ public class TesteDeteccaoFace extends Activity {
 		 Matrix matrix = new Matrix();
 		 Bitmap girado = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
 		 matrix, true);
-		 return new BitmapDrawable(girado);
-		 }
+		
+		BitmapDrawable bd = new BitmapDrawable(girado);
+		myImageView = new MyImageView(this);
+		myImageView.setBackgroundDrawable(bd);
+		setContentView(myImageView);
+		
+		
+		Bitmap camisa = BitmapFactory.decodeResource(getResources(), R.drawable.camisa_preta);
+		Matrix matrix1 = new Matrix();
+		Bitmap girado1 = Bitmap.createBitmap(camisa, 0, 0, camisa.getWidth(), camisa.getHeight(), matrix1, true);
+		
+		Face face = arrayFaces[0];
+		PointF point = new PointF();
+		face.getMidPoint(point);
+		
+		System.out.println("x: "+point.x);
+		System.out.println((int)point.x);
+		System.out.println("y: " + point.y);
+		System.out.println((int)point.y);
+		
+		layout = new LinearLayout(this);
+		layout.scrollTo(3*(int)point.x, 100+(int)point.y);
+		layout.setBackgroundDrawable(new BitmapDrawable(girado1));
+//	    layout.setBackgroundResource(R.drawable.camisa_preta);
+	        
+	    addContentView(layout, new LayoutParams(camisa.getWidth(), camisa.getHeight()));
+
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+	}
+	
+//	private BitmapDrawable carregaDrawable() {
+//		 Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.pessoa2);
+//		 
+//		 FaceDetector f = new FaceDetector(b.getWidth(), b.getHeight(), 1);
+//		 arrayFaces = new Face[1];
+//		 int numFace = f.findFaces(b,arrayFaces);
+//		
+//		
+//		 System.err.println("-------------- aqui: numero de faces: " + numFace);
+//		
+//		
+//		 Matrix matrix = new Matrix();
+//		 Bitmap girado = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
+//		 matrix, true);
+//		 return new BitmapDrawable(girado);
+//		 }
 	
 	
 	public class MyImageView extends View {
@@ -68,10 +107,13 @@ public class TesteDeteccaoFace extends Activity {
 //			Face face = arrayFaces[0];
 //			PointF point = new PointF();
 //			face.getMidPoint(point);
-			 
+//			
 //			roupaSuperior = new BitmapDrawable(girado);
-//			roupaSuperior.setBounds(point.x, (point.x + camisa.getWidth()), point.y, (point.y + camisa.getHeight()));
-
+////			int f = (int) point.x + camisa.getWidth();
+////			int g = (int) point.y + camisa.getHeight();
+////			roupaSuperior.setBounds(f, (int)point.x, g,(int)point.y);
+//			invalidate();
+			
 			this.setFocusable(true);
 			this.requestFocus();
 			this.setFocusableInTouchMode(true);
