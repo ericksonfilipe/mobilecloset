40a80188466a479b961f6754a722d70c42a476fd
Janderson <janderson.jason@gmail.com>
2012-06-01 14:51:06 -0300

    cabide balan?ando ;D

--- src/br/edu/ufcg/MobileclosetActivity.java
+++ src/br/edu/ufcg/MobileclosetActivity.java
@@ -1,21 +1,21 @@
 package br.edu.ufcg;
 
 import java.io.ByteArrayOutputStream;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Bitmap.CompressFormat;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import br.edu.ufcg.BD.BDAdapter;
@@ -23,19 +23,31 @@ import br.edu.ufcg.model.Calibragem;
 import br.edu.ufcg.model.Categoria;
 import br.edu.ufcg.model.Roupa;
 
-public class MobileclosetActivity extends Activity implements OnClickListener {
+public class MobileclosetActivity extends Activity {
 	/** Called when the activity is first created. */
 
 	private BDAdapter dao;
 	
-	//criado para a v2
+	private AnimationDrawable animation;
+	
+	public void onWindowFocusChanged(boolean hasFocus) {
+		if (hasFocus) {
+			animation.start();
+		} else {
+			animation.stop();
+		}
+		
+	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_v2);
-
-		Button bIniciar = (Button) findViewById(R.id.button_iniciar);
-		bIniciar.setOnClickListener(this);
+		
+		ImageView spin_animation = new ImageView(this);  
+	    setContentView(spin_animation);  
+	    spin_animation.setBackgroundResource(R.anim.spin_animation);  
+	    this.animation = (AnimationDrawable) spin_animation.getBackground();  
+		
 		
 		dao = new BDAdapter(this);
 		
@@ -109,9 +121,43 @@ public class MobileclosetActivity extends Activity implements OnClickListener {
 		}
 		
 
-//		Button bOpcoes = (Button) findViewById(R.id.button_opcoes_v2);
-//		bOpcoes.setOnClickListener(this);
+		//Botao Iniciar
+		ImageButton bIniciar = new ImageButton(this);
+	    bIniciar.setPadding(0, 0, 10, 10);
+		bIniciar.setImageResource(R.drawable.button_iniciar);
+		bIniciar.setBackgroundColor(Color.TRANSPARENT);
+		bIniciar.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent i;
+				if (dao.getRoupas().isEmpty()) {
+					i = new Intent(v.getContext(), OpcoesActivity.class);
+				} else {
+					i = new Intent(v.getContext(), ProvadorActivity.class);
+					//			if (dao.getManequimPadrao() == null) {
+//						Toast.makeText(this, "Não há manequim escolhido!", Toast.LENGTH_SHORT).show();
+//					} else {
+//						i.putExtra("background", dao.getManequimPadrao());
+//						startActivity(i);
+//					}
+					i.putExtra("background", dao.getManequimPadrao());
+					startActivity(i);
+				}
+				startActivity(i);
+			}
+		});
 		
+				
+		RelativeLayout layoutIniciar = new RelativeLayout(this);
+		LinearLayout linearIniciar = new LinearLayout(this);
+		linearIniciar.addView(bIniciar);
+		linearIniciar.setGravity(Gravity.RIGHT);
+		linearIniciar.setLayoutParams(new LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		layoutIniciar.addView(linearIniciar);
+		layoutIniciar.setGravity(Gravity.BOTTOM);
+		addContentView(layoutIniciar, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		
+		//Botao Fechar
 		ImageButton closeButton = new ImageButton(this);
 		closeButton.setImageResource(R.drawable.close);
 		closeButton.setBackgroundColor(Color.TRANSPARENT);
@@ -133,88 +179,6 @@ public class MobileclosetActivity extends Activity implements OnClickListener {
 
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}	
-
-	//criado para v2
-	//@Override
-	public void onClick(View v) {
-		Intent i;
-		if (v.getId() == R.id.button_iniciar) {
-			if (dao.getRoupas().isEmpty()) {
-				i = new Intent(v.getContext(), OpcoesActivity.class);
-				startActivity(i);
-			} else {
-				i = new Intent(v.getContext(), ProvadorActivity.class);
-				i.putExtra("background", dao.getManequimPadrao());				
-				startActivity(i);
-			}
-		}
-	}	
-
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.main);
-//		
-//		Button bTeste = (Button) findViewById(R.id.button_teste);
-//		bTeste.setOnClickListener(this);
-//
-//		Button bManequim = (Button) findViewById(R.id.button_manequim);
-//		bManequim.setOnClickListener(this);
-//
-//		Button bRoupas = (Button) findViewById(R.id.button_roupas);
-//		bRoupas.setOnClickListener(this);
-//		
-//		Button bProvar = (Button) findViewById(R.id.button_provar);
-//		bProvar.setOnClickListener(this);
-//
-//		Button bSair = (Button) findViewById(R.id.button_sair);
-//		bSair.setOnClickListener(this);
-//
-//		Button bCreditos = (Button) findViewById(R.id.button_creditos);
-//		bCreditos.setOnClickListener(this);
-//
-//		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-//	}	
 	
-	
-//	//@Override
-//	public void onClick(View v) {
-//		Intent i;
-//		switch (v.getId()) {
-//		case R.id.button_manequim:
-//			i = new Intent(v.getContext(), ManequimActivity.class);
-//			startActivity(i);
-//			break;
-//		case R.id.button_roupas:
-//			i = new Intent(v.getContext(), RoupasActivity.class);
-//			startActivity(i);
-//			break;
-//		case R.id.button_provar:
-//			i = new Intent(v.getContext(), ProvadorActivity.class);
-//			BDAdapter dao = new BDAdapter(this);
-//			if (dao.getManequimPadrao() == null) {
-////				i.putExtra("manequimFaltando", true);
-////				startActivity(i);
-//				Toast.makeText(this, "Não há manequim escolhido!", Toast.LENGTH_LONG).show();
-//			} else {
-//				i.putExtra("background", dao.getManequimPadrao());
-//				startActivity(i);
-//			}
-//			break;
-//		case R.id.button_creditos:
-//			i = new Intent(v.getContext(), Creditos.class);
-//			startActivity(i);
-//			break;
-//		case R.id.button_sair:
-//			finish();
-//			break;
-//		case R.id.button_teste:
-//			i = new Intent(v.getContext(), TesteDeteccaoFace.class);
-//			startActivity(i);
-//			break;
-//		}
-//	}
-	
-
 	
 }
